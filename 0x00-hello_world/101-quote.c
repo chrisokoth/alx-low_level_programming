d3861d4ba7874b0c84249d4529187dd35a362875
chrisokoth <chrisokoth17@gmail.com>
2022-09-09 06:44:35 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * * main - prints and that piece of art is useful - Dora Korpar, 2015-10-19,
  *                followed by a new line, to standard error.
  *
  * Return: 1 if successful.
@@ -10,7 +10,8 @@
 int main(void)
 {
 	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      and that piece of art is useful\
+	      - Dora Korpar, 2015-10-19\n,
 	      59);
 	return (1);
 }

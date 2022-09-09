c75ed970de7dc7a0e5be1ee647b5ccc469b7098b
chrisokoth <chrisokoth17@gmail.com>
2022-09-09 06:01:14 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -10,6 +9,6 @@
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

dbb430bc74c4bb705eb2b73fa810a0b2c6cdd4e2
chrisokoth <chrisokoth17@gmail.com>
2022-09-22 07:30:56 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is my README for my extra string pointers and arrays
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _strcat - function to concatenates
+ * @dest: returning pointer to dest
+ * @src: the one to concatenates
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);

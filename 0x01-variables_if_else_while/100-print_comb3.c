761d692d7823eba83a7aaa8a7f9a00a8d0d7b3ba
chrisokoth <chrisokoth17@gmail.com>
2023-01-26 12:32:25 -0800

    corrections

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,44 +1,32 @@
 #include <stdio.h>
-
 /**
- * main - Prints all possible combinations of two different digits,
- *        in ascending order, separated by a comma followed by a space.
+ * main - Entry point
  *
- * Return: 0 if successful, non-zero otherwise.
- * Return: Always 0.
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 {
-	int digit1 = 0;
-	int digit2;
-	int digit1, digit2;
+	int i;
+	int j;
 
-	while (digit1 < 9)
-	for (digit1 = 0; digit1 < 9; digit1++)
+	for (i = 48; i < 58; i++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 8 && digit2 == 9)
+		for (j = 48; j < 58; j++)
+		{	
+			if (i != j && i < j)
 			{
-				break;
+				putchar(i);
+				putchar(j);
+				if (j == 57 && i == 56)
+				{
+					break
+						;
+				}
+				putchar(',');
+				putchar(' ');
 			}
-			else
-			{
-				putchar(44);
-				putchar(32);
-			}
-				continue;
-
-			putchar(',');
-			putchar(' ');
 		}
-		digit1++;
 	}
-
 	putchar('\n');
-
 	return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,36 +1,23 @@
 #include <stdio.h>
+
 /**
- * main - Prints all possible combinations of single-digit numbers.
+ * main - Entry point
  *
- * Return: 0 if successful, non-zero otherwise.
- * Return: Always 0.
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 {
-	int num = 0;
-	int num;
+	int i;
 
-	while (1)
-	for (num = 0; num <= 9; num++)
+	for (i = 48; i < 58; i++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
+		putchar(i);
+		if (i != 57)
 		{
-			break;
+			putchar(',');
+			putchar(' ');
 		}
-		else
-		{
-			putchar(44);
-			putchar(32);
-		}
-		num++;
-			continue;
-
-		putchar(',');
-		putchar(' ');
 	}
-
 	putchar('\n');
-
 	return (0);
 }

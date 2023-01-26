f0004e26f334cb0b1ede3d579c307ec5ba82e4d7
chrisokoth <chrisokoth17@gmail.com>
2023-01-26 12:18:49 -0800

    corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * * main - prints and that piece of art is useful - Dora Korpar, 2015-10-19,
- *                followed by a new line, to standard error.
+ * main - Entry point
  *
- * Return: 1 if successful.
- * Return: Always 1.
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-	write(2,
-	      and that piece of art is useful\
-	      - Dora Korpar, 2015-10-19\n,
-	      59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-
 /**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
-* Return: 0
-*/
+ * main - entry
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+printf("Size of an int: %i byte(s)\n", sizeof(int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long int)); 
+printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+printf("Size of a float: %i byte(s)\n", sizeof(float));
 
-	
-
+return (0);
 }
